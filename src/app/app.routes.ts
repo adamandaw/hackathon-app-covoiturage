d1eedbc16868973772c4db4892e82b0df73eb2b0
Mohamed <mohamedndaw697@gmail.com>
2024-06-08 19:46:32 +0000

    Page Login + DashBoard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "",
+        loadChildren: ()=> import("./public/public.module").then(
+            currentModule => currentModule.PublicModule
+        )
+    },
+    {
+        path:"app/user",
+        loadChildren:()=>import("./secure/secure.module").then(mod=>mod.SecureModule),
+        // canMatch:[()=>inject(AuthentificateService).isAuthentificated]
+      },
+];
